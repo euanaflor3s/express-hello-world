9c62196d99b1a7728128afe1e991244ef433a54e
euanaflor3s <anacarlafloressilva@gmail.com>
2023-12-08 14:53:19 -0400

    passo 2

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ app.get('/req', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
 app.get('/meunome', (req, res) => {
-    console.log('Meu nome é Ana Carla Flores Da Silva! :)')
+  res.send('Meu nome é Ana Carla Flores Da Silva! :)')
 })
+
+app.get('/tico', (req, res) => {
+  res.send('teco')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
